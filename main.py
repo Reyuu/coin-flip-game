dbfa9803b5003bc87f111d5b753e6be4263d6472
Rey <rey444xd3@gmail.com>
2017-04-13 22:27:50 +0200

    added handling font from png, issue: some letters doesn't work

--- main.py
+++ main.py
@@ -5,6 +5,61 @@ import math
 
 #TODO: handle bets, choose t and h side
 
+def load_png(name):
+    """
+    Load image and return image object
+    """
+    try:
+        image = pygame.image.load(name)
+        if image.get_alpha() is None:
+            image = image.convert()
+        else:
+            image = image.convert_alpha()
+    except pygame.error:
+        print("Cannot load image: %s" % name)
+    return image, image.get_rect()
+
+class FontHandling:
+    def __init__(self, name, letter_order, width, height):
+        self.image = load_png(name)[0]
+        self.letters = []
+        for i in letter_order:
+            self.letters += i
+        self.letters_images = []
+        self.width = width
+        self.height = height
+        self.letters_d = {}
+        self.split_image()
+
+    def render(self, text):
+        #create surface (consider self.width and self.height)
+        #put letters ~2px apart of each
+        #if space then move whole self.width+2
+        #ignore any punctuation marks
+        bl = 2
+        current_x = self.width
+        surf = pygame.Surface(((self.width+bl)*len(text)+self.width, self.height))
+        for i in text:
+            if i in self.letters_d.keys():
+                print(i)
+                surf.blit(self.letters_d[i], (current_x, 0))
+                current_x += self.width+bl
+            elif i == " ":
+                current_x += self.width+bl
+            else:
+                pass
+        return surf
+   
+    def split_image(self):
+        for x in range(len(self.letters)):
+            rect = (0+self.width*x, 0, self.width, self.height)
+            surface = pygame.Surface((self.width, self.height)).convert_alpha()
+            surface.blit(self.image, (0, 0), area=rect)
+            self.letters_images += [surface]
+        for i, j in zip(self.letters, self.letters_images):
+            self.letters_d.update({i: j})
+            #print(self.letters_d)
+
 def isPointInsideRect(pos, rect):
     """
     Checks if point is inside some rectangle (Rect object)
@@ -71,7 +126,8 @@ class GameScene(ezpygame.Scene):
         self.bet = 0
         self.current_bet = "tails"
         self.bet_rate = 0.95
-        self.font = pygame.font.SysFont("Monospace", size=12)
+        self.font = FontHandling("font.png", "qwertyuiopasdfghjklzxcvbnm1234567890", 12, 16)
+        #self.font = pygame.font.SysFont("Monospace", size=12)
 
     def on_enter(self, previous_scene):
         self.application.title = "Game(2)"
@@ -84,41 +140,42 @@ class GameScene(ezpygame.Scene):
                 if self.coin.sides[self.coin.side] == self.current_bet:
                     self.currency.amount += int(self.bet*self.bet_rate)
                 else:
-                   self.currency.amount -= self.bet 
+                   self.currency.amount -= self.bet
+                   if self.currency.amount < 0:
+                       self.currency.amount = 0
             if isPointInsideRect(pos, (10, 10, 10, 10)):
                 if self.currency.amount > self.bet+1:
                     self.bet += 1
-            if isPointInsideRect(pos, (28, 10, 10, 10)):
+            if isPointInsideRect(pos, (10, 28, 10, 10)):
                 if 0 <= self.bet-1:
                     self.bet -= 1
-            if isPointInsideRect(pos, (28+18, 10, 10, 10)):
+            if isPointInsideRect(pos, (10, 28+18, 10, 10)):
                 self.current_bet = "tails"
-            if isPointInsideRect(pos, (28+2*18, 10, 10, 10)):
+            if isPointInsideRect(pos, (10, 28+2*18, 10, 10)):
                 self.current_bet = "heads"
-
 
     def draw(self, screen):
         screen.fill((0,0,0))
         pygame.draw.rect(screen, (250, 5, 5), (10, 10, 10, 10))
-        pygame.draw.rect(screen, (250, 5, 5), (28, 10, 10, 10))
-        screen.blit(self.font.render("+", True, (255, 255, 255)), (10, 10))
-        screen.blit(self.font.render("-", True, (255, 255, 255)), (28, 10))
-        screen.blit(self.font.render("tails", True, (255, 255, 255)), (28+18, 10))
-        screen.blit(self.font.render("heads", True, (255, 255, 255)), (28+2*18, 10))
+        pygame.draw.rect(screen, (250, 5, 5), (10, 28, 10, 10))
+        screen.blit(self.font.render("+"), (10, 10))
+        screen.blit(self.font.render("-"), (10, 28))
+        screen.blit(self.font.render("tails"), (10, 28+18))
+        screen.blit(self.font.render("heads"), (10, 28+2*18))
         if self.current_bet == "tails":
-            pygame.draw.rect(screen, (0, 255, 0), (28+18, 10, 10, 10))
+            pygame.draw.rect(screen, (0, 255, 0), (10, 28+18, 10, 10))
         else:
-            pygame.draw.rect(screen, (255, 0, 0), (28+18, 10, 10, 10))
+            pygame.draw.rect(screen, (255, 0, 0), (10, 28+18, 10, 10))
 
         if self.current_bet == "heads":
-            pygame.draw.rect(screen, (0, 255, 0), (28+2*18, 10, 10, 10))
+            pygame.draw.rect(screen, (0, 255, 0), (10, 28+2*18, 10, 10))
         else:
-            pygame.draw.rect(screen, (255, 0, 0), (28+2*18, 10, 10, 10))
+            pygame.draw.rect(screen, (255, 0, 0), (10, 28+2*18, 10, 10))
 
         pygame.draw.rect(screen, (255, 0, 255), (100, 100, 100, 100))
-        screen.blit(self.font.render("Coin flip", True, (255, 255, 255)), (150, 150))
-        screen.blit(self.font.render("Coin side: %s" % self.coin.sides[self.coin.side], True, (255, 255, 255)), (0, 400))
-        screen.blit(self.font.render("Bet: %s  Flowers: %s" % (self.bet, self.currency.amount), True, (255, 255, 255)), (0, 416))
+        screen.blit(self.font.render("Coin flip"), (150, 150))
+        screen.blit(self.font.render("Coin side: %s" % self.coin.sides[self.coin.side]), (0, 400))
+        screen.blit(self.font.render("Bet: %s  Flowers: %s" % (self.bet, self.currency.amount)), (0, 416))
 
 
 app = ezpygame.Application(
